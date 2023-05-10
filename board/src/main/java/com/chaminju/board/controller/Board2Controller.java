76218effd3fa3a4df69d0b5f653b537887374f73
minju <ckalswn007@naver.com>
2023-05-10 23:29:15 +0900

    2023.05.10 23:30

--- board/src/main/java/com/chaminju/board/controller/Board2Controller.java
+++ board/src/main/java/com/chaminju/board/controller/Board2Controller.java
@@ -2,7 +2,6 @@ package com.chaminju.board.controller;
 
 import javax.validation.Valid;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
